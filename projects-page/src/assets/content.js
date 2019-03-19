0200c7e8d5c034a46df566a98545ed738224a9d4
Natalie Fung <nataliecfung@gmail.com>
2019-03-19 19:51:56 -0400

    edits

--- projects-page/src/assets/content.js
+++ projects-page/src/assets/content.js
@@ -3,8 +3,8 @@ import image_1 from '../screens/Pumpkin/images/helicopter.png';
 const content = [
     {
         id: 0,
-        title: 'Week in Photos',
-        description: '...',
+        title: 'This Week in Photos',
+        description: 'The best shots by our photo department this past week',
         image: 'https://cornellsun.com/wp-content/uploads/2019/02/BYT-Snow-9.jpg',
         url: '/week-in-photos'
     },
@@ -17,4 +17,4 @@ const content = [
     },
 ];
 
-export default content; 
+export default content;
