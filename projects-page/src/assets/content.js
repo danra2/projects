02c0b44a7bbbd9741a16c7c29964dc8dd670eff6
Jason Huang <jh2424@cornell.edu>
2019-03-19 13:10:41 -0400

    pumpkin

--- projects-page/src/assets/content.js
+++ projects-page/src/assets/content.js
@@ -2,19 +2,19 @@ import image_1 from '../screens/Pumpkin/images/helicopter.png';
 
 const content = [
     {
-        id: 0, 
-        title: 'Week in Photos', 
-        description: '...', 
-        image: 'https://cornellsun.com/wp-content/uploads/2019/02/BYT-Snow-9.jpg', 
+        id: 0,
+        title: 'Week in Photos',
+        description: '...',
+        image: 'https://cornellsun.com/wp-content/uploads/2019/02/BYT-Snow-9.jpg',
         url: '/week-in-photos'
-    }, 
+    },
     {
-        id: 1, 
-        title: 'How The Pumpkin Got on the Clocktower', 
-        description: '...', 
-        image: image_1, 
+        id: 1,
+        title: 'How The Pumpkin Got on the Clocktower',
+        description: 'Re-solving The Greatest Mystery In Cornell History',
+        image: image_1,
         url: '/pumpkin'
-    }, 
-]; 
+    },
+];
 
 export default content; 
