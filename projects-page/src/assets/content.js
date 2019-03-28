114d76be35fba9dc322458df2be548fdb9b3d71e
Natalie Fung <nataliecfung@gmail.com>
2019-03-27 23:21:39 -0400

    add huawei feature

--- projects-page/src/assets/content.js
+++ projects-page/src/assets/content.js
@@ -1,15 +1,23 @@
 import image_1 from '../screens/Pumpkin/images/helicopter.png';
+import image_2 from '../screens/Huawei/images/huaweicover.jpg';
 
 const content = [
     {
-        id: 0,
+        id: 0, 
+        title: 'In 2017, Huawei Paid Cornell $5.3 Million for Research Contracts. That’s More Than The Embattled Tech Firm Paid to Any U.S. College in the Last Six Academic Years.', 
+        description: 'Four leading western universities have canceled research collaborations with Huawei or suspended talks for new deals with the company.', 
+        image: image_2, 
+        url: '/huawei'
+    },
+    {
+        id: 1,
         title: 'This Week in Photos',
         description: 'The best shots by our photo department this past week',
         image: 'https://cornellsun.com/wp-content/uploads/2019/02/BYT-Snow-9.jpg',
         url: '/week-in-photos'
     },
     {
-        id: 1, 
+        id: 2, 
         title: 'How The Pumpkin Got on the Clocktower', 
         description: 'Re-solving The Greatest Mystery In Cornell History', 
         image: image_1, 
