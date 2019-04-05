8ff73f6abc63365d6945e66481e82a29953ee333
jasonh90 <jasonh402@gmail.com>
2019-04-05 17:42:39 -0400

    warnings

--- projects-page/src/screens/PhotoWeek/GalleryImage.js
+++ projects-page/src/screens/PhotoWeek/GalleryImage.js
@@ -1,38 +1,34 @@
 import React from "react";
-import './PhotoWeek.css';
+import "./PhotoWeek.css";
 
 const cont = {
-    backgroundColor: "#eee",
-    cursor: "pointer",
-    overflow: "hidden",
-    position: "relative"
+  backgroundColor: "#eee",
+  cursor: "pointer",
+  overflow: "hidden",
+  position: "relative"
 };
 
-const GalleryImage = ({
-    photo,
-    onClick,
-    direction,
-    index,
-    top,
-    left
-  }) => {
+const GalleryImage = ({ photo, onClick, direction, index, top, left }) => {
+  if (direction === "column") {
+    cont.position = "absolute";
+    cont.left = left;
+    cont.top = top;
+  }
 
-    if (direction === "column") {
-      cont.position = "absolute";
-      cont.left = left;
-      cont.top = top;
-    }
-
-    return (
-      <div className="photo-block">
-        <img className="photo-img" {...photo} onClick={e => onClick(e, { index, photo })}/>
-        <div className="photo-description"> 
+  return (
+    <div className="photo-block">
+      <img
+        className="photo-img"
+        {...photo}
+        onClick={e => onClick(e, { index, photo })}
+        alt=""
+      />
+      <div className="photo-description">
         <p className="photo-caption"> {photo.caption} </p>
         <p className="photo-credit"> {photo.credit} </p>
-        </div>
       </div>
-    );
-  };
-  
-  export default GalleryImage;
-  
+    </div>
+  );
+};
+
+export default GalleryImage;
